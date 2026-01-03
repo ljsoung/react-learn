8101790fdddd2344b3d147fc6aa01f4da088c9d6
ljsoung <super15600@gmail.com>
2026-01-04 03:16:27 +0900

    이벤트 처리 학습

--- react01-basic/src/App-04event-1.jsx
+++ react01-basic/src/App-04event-1.jsx
@@ -14,21 +14,6 @@ function FrontComp(props){
   </>)
 }
 
-const BackComp = ({onMyEvent2}) => {
-  return (<>
-    <li><a href="/" onClick={(event) => {
-      event.preventDefault();
-      onMyEvent2('백엔드 클릭됨(자식전달)');
-    }}>백엔드</a></li>
-    <ul>
-      <li>Java</li>
-      <li>Oracle</li>
-      <li>JSP</li>
-      <li>Spring Boot</li>
-    </ul>
-  </>) 
-}
-
 function App() {
   return (<>
     <h2>React-Event</h2>
@@ -37,9 +22,6 @@ function App() {
       <FrontComp onMyEvent1={() =>{
         alert('프론트엔드 클릭됨(부모전달)');
       }}></FrontComp>
-      <BackComp onMyEvent2={(msg) => {
-        alert(msg);
-      }} />
     </ol>
   </>)
 }
